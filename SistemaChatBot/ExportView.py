4a68c3669ad4a3a6349744e0c59d4a51365496e7
sammycosta <arminetah@hotmail.com>
2022-02-15 11:15:34 -0300

    View da conversa com botoes de export e import e view de import e export

---
+++ SistemaChatBot/ExportView.py
@@ -0,0 +1,19 @@
+
+
+import PySimpleGUI as sg
+import BaseView
+
+# View do padrão MVC
+
+
+class ExportView (BaseView):
+    def __init__(self, controlador):
+        super().__init__("Exportação de chatbot", controlador)
+
+    def tela_consulta(self):
+        linha0 = [sg.Text('Digite o nome do arquivo que quer salvar:')]
+        linha1 = [sg.InputText('', key='caminho_export')]
+        linha2 = [sg.Button('Exportar')]
+
+        self.__container = [linha0, linha1, linha2]
+        self.update_layout(self.__container)
--- SistemaChatBot/SistemaChatBotView.py
+++ SistemaChatBot/SistemaChatBotView.py
@@ -1,9 +1,10 @@
 import PySimpleGUI as sg
-import SistemaChatBotController
+from SistemaChatBot.BaseView import BaseView
+# import SistemaChatBotController
 # View do padrão MVC
 
 
-class SistemaChatBotView():
+class SistemaChatBotView(BaseView):
     def __init__(self, controlador, bot):
         self.__controlador = controlador
         self.__container = []
@@ -13,28 +14,17 @@ class SistemaChatBotView():
 
     def tela_chatbot(self):
         sg.theme('Dark Brown 1')
+        for comando in self.__bot.comandos:
+            self.__container.append([sg.Button(comando.comando)])
 
-        self.__container = [
-            [sg.Text('')],
-            # botões com os comandos do BOT
-            for comando in self.__bot.comandos:
-                [sg.Button(comando.comando)]
-            # no controller: ele detecta o comando pelo comando.comando e me passa a resposta via pega_resposta no mostra_resultado
-            # area de resposta do bot
-            [sg.Text('', key='resposta', size=(50, 1))]
-        ]
+        self.__container.append([sg.Text('', key='resposta', size=(50, 1))])
 
-        self.__window = sg.Window(
-            "Sistema Chat Bot", self.__container, font=("Helvetica", 14))
+        self.__container.append([sg.Button('Exportar'), sg.Button('Importar')])
+
+        self.update_layout(self.__container)
 
     def mostra_resultado(self, resposta):
         self.__window.Element('resposta').Update(resposta)
 
     def limpa_lacuna(self, key):
         self.__window.Element(key).Update('')
-
-    def le_eventos(self):
-        return self.__window.read()
-
-    def fim(self):
-        self.__window.close()
